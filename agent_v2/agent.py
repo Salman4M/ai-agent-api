1529cb8a63b67e6e410a1d37125978ca8ab7dcf0
Salman4M <salmanmammadli@hotmail.com>
2026-03-11 17:38:56 +0400

    chore: unmodified but hasn't commited yet files

--- agent_v2/agent.py
+++ agent_v2/agent.py
@@ -1,15 +1,16 @@
 import os
 from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
 from agent_v2.tools import web_search,calculator,code_executor
 
 def create_agent()->Agent:
     return Agent(
         name="research_agent",
-        model="gemini-2.0-flash",
+        model=LiteLlm(model="groq/llama-3.3-70b-versatile"),
         description="A research agent that can search the web, do math and execute code.",
         instruction="""You are a helpful research assistant.
         When asked a question:
-        - User web_search for current information or facts
+        - Use web_search for current information or facts
         - Use calculator for any mathematical operations
         - Use code_executor to run Python code when needed
         - Always provide a clear, complete final answer
@@ -19,3 +20,5 @@ def create_agent()->Agent:
     )
 
 agent = create_agent()
+
+root_agent = agent
